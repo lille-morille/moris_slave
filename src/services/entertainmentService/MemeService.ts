d076afd944ea5428fb0d86f3b82134bc3e326b32
Anders <anders.morille@gmail.com>
2023-08-30 14:02:33 +0200

    Helped Borgar

---
+++ src/services/entertainmentService/MemeService.ts
@@ -0,0 +1,13 @@
+import { CommandInteraction } from "discord.js";
+
+export default class MemeService {
+  private interaction: CommandInteraction;
+
+  constructor(interaction: CommandInteraction) {
+    this.interaction = interaction;
+  }
+
+  public async getMeme() {
+    this.interaction.reply({ content: "Here is a meme!", ephemeral: false });
+  }
+}
--- src/services/eventHandlers/handleInteractionCreate.ts
+++ src/services/eventHandlers/handleInteractionCreate.ts
@@ -1,18 +1,18 @@
-import { CacheType, Interaction } from "discord.js";
+import { Interaction } from "discord.js";
 import {
   ADD_HELPER_SUBJECT_COMMAND,
   BECOME_HELPER_COMMAND,
   MEME_COMMAND,
   REMOVE_HELPER_COMMAND,
-  SOLVED_COMMAND
+  SOLVED_COMMAND,
 } from "../../constants/commands";
-import SubjectHelperService from "../subjectHelperService/SubjectHelperService";
 import MemeService from "../entertainmentService/MemeService";
+import SubjectHelperService from "../subjectHelperService/SubjectHelperService";
 
 /**
  * Handles all incoming command interactions (slash commands) from users
  */
-export default async function (interaction: Interaction<CacheType>) {
+export default async function (interaction: Interaction) {
   if (!interaction.isChatInputCommand()) return;
 
   const helperService = new SubjectHelperService(interaction);
@@ -28,9 +28,10 @@ export default async function (interaction: Interaction<CacheType>) {
       break;
     case SOLVED_COMMAND:
       helperService.handleSolved();
-      case MEME_COMMAND:
-        MemeService.getMeme();
-        break;
+      break;
+    case MEME_COMMAND:
+      new MemeService(interaction).getMeme();
+      break;
     default:
       interaction.reply({ content: "Unknown command", ephemeral: true });
       break;
