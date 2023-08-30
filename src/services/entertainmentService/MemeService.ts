792829cdf332c1bb4759ab80d9dff540ff231a4e
borgaar <142639383+borgaar@users.noreply.github.com>
2023-08-30 14:48:07 +0200

    modified:   src/services/entertainmentService/MemeService.ts new file:   src/services/entertainmentService/types.ts

--- src/services/entertainmentService/MemeService.ts
+++ src/services/entertainmentService/MemeService.ts
@@ -1,4 +1,5 @@
 import { CommandInteraction } from "discord.js";
+import {ResponseBody} from "./types";
 
 export default class MemeService {
   private interaction: CommandInteraction;
@@ -8,6 +9,14 @@ export default class MemeService {
   }
 
   public async getMeme() {
-    this.interaction.reply({ content: "Here is a meme!", ephemeral: false });
+    const response = await fetch("https://meme-api.com/gimme");
+
+    if (response.status === 200) {
+      
+      const body: ResponseBody = await response.json()
+      this.interaction.reply({ content: body["url"], ephemeral: false });
+    }
+
+    // this.interaction.reply({ content: "Here is a meme!", ephemeral: false });
   }
 }
