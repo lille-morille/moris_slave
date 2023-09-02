cc10bd338caf4640f199254ff2e75909c1a2e872
Anders <anders.morille@gmail.com>
2023-09-02 13:12:34 +0200

    Added warning if missing AUTH_TOKEN

--- src/server.ts
+++ src/server.ts
@@ -6,6 +6,8 @@ import "./services/chatCommands/deployCommands";
 import handleInteractionCreate from "./services/eventHandlers/handleInteractionCreate";
 import handleThreadCreate from "./services/eventHandlers/handleThreadCreate";
 
+if (!process.env.AUTH_TOKEN) console.error("No AUTH_TOKEN provided. Please provide one in the .env file.");
+
 const client = new Client({ intents: [GatewayIntentBits.Guilds] });
 export default client;
 
@@ -13,4 +15,4 @@ client.on("ready", () => console.log(`Logged in as ${client.user.tag}!`));
 client.on("interactionCreate", handleInteractionCreate);
 client.on("threadCreate", handleThreadCreate);
 
-client.login(process.env.AUTH_TOKEN);
+client.login(process.env.AUTH_TOKEN).then(r => console.log(r));
